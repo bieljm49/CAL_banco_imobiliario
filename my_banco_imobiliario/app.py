45f98021127d9c8b27c891ffee3b474337187eb3
Gabriel Araujo <gabrielamedeiros49@gmail.com>
2024-11-01 15:44:08 -0300

    Para implantação
    Para implantação
    requirements.txt
    Procfile

--- my_banco_imobiliario/app.py
+++ my_banco_imobiliario/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
 app = Flask(__name__)
 app.secret_key = 'secreta123'  # chave de segurança da sessão
 
