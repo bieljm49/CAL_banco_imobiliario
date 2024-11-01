772fb870845c5c7184519034001c33101df8b843
Gabriel Araujo <gabrielamedeiros49@gmail.com>
2024-11-01 15:52:38 -0300

    Update app.py
    comentario porta

--- my_banco_imobiliario/app.py
+++ my_banco_imobiliario/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-import os
+#import os
 #port = int(os.environ.get("PORT", 5000))
 #app.run(host='0.0.0.0', port=port)
 
